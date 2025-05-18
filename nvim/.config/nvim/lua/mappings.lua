c2bdee8f734cff35f794ff1ff58902348b4908ed
Koki Kato <kato@shanord.com>
2025-05-18 14:14:27 +0900

    :+1: feat: Neovim設定をNvChadベースで初期構築
    - `NvChad`を利用したカスタム設定を追加
    - 必要なプラグインを「Lazy.nvim」で管理（例: Noice, Mason, Treesitter, Telescopeなど）
    - LSPおよびフォーマッタの設定追加（例: HTML, CSS, Lua用）
    - キーマッピング設定の作成およびカスタマイズ
    - スタイル設定用`.stylua.toml`の追加
    - 基本的なドキュメント（README, LICENSE）の追加

---
+++ nvim/.config/nvim/lua/mappings.lua
@@ -0,0 +1,80 @@
+require "nvchad.mappings"
+
+local map = vim.keymap.set
+local nomap = vim.keymap.del
+
+-- 標準設定変更・削除
+nomap("n", "<tab>") -- <tab>解除（Ctrl+i有効化）
+
+-- モード共通のマッピング
+map({ "n", "v" }, "<C-a>", "^", { desc = "Move to line start" })
+map({ "n", "v" }, "<C-e>", "$", { desc = "Move to line end" })
+map({ "n", "v" }, "H", "^", { desc = "Move to line start" })
+map({ "n", "v" }, "L", "$", { desc = "Move to line end" })
+
+-- ノーマルモード
+map("n", ";", ":", { desc = "CMD enter command mode" })
+map("n", "vv", "V", { desc = "Select whole line" })
+map("n", "<CR>", "i<CR><ESC>", { desc = "Insert new line" })
+map("n", "+", "<C-a>", { desc = "Increment number" })
+map("n", "-", "<C-x>", { desc = "Decrement number" })
+map("n", "x", '"_x', { desc = "Delete without yanking" })
+map("n", "<C-j>", ":bn<CR>", { desc = "Next buffer" })
+map("n", "<C-k>", ":bp<CR>", { desc = "Previous buffer" })
+map("n", "<C-l>", ":bd<CR>", { desc = "Close buffer" })
+
+-- NvimTreeがロードされた後にグローバルEscマッピングも設定
+map("n", "<Esc>", function()
+  if vim.bo.filetype == "NvimTree" then
+    vim.cmd("wincmd l")
+  end
+end, { desc = "Exit NvimTree to main pane", silent = true })
+
+-- 保存・終了
+map("n", "<Leader>w", ":w<CR>", { desc = "Save file" })
+map("n", "<Leader>l", ":bd<CR>", { desc = "Close buffer" })
+map("n", "<Leader>x", ":x<CR>", { desc = "Save and quit" })
+map("n", "<Leader>q", ":quit<CR>", { desc = "Quit" })
+map("n", "<Leader>qq", ":qa!<CR>", { desc = "Force quit all" })
+
+-- ファイル・バッファ操作
+map("n", "<Leader>n", "<cmd>Telescope find_files<cr>", { desc = "Telescope find files" })
+map("n", "<Leader>.", function()
+  require("nvchad.tabufline").next()
+end, { desc = "Buffer goto next" })
+map("n", "<Leader>,", function()
+  require("nvchad.tabufline").prev()
+end, { desc = "Buffer goto prev" })
+map("n", "<Leader>x", function()
+  require("nvchad.tabufline").close_buffer()
+end, { desc = "Buffer close" })
+
+-- スプリット操作
+map("n", "ss", "<cmd>split<CR>", { desc = "Horizontal split" })
+map("n", "sv", "<cmd>vsplit<CR>", { desc = "Vertical split" })
+map("n", "sh", "<cmd>wincmd h<CR>", { desc = "Move to left split" })
+map("n", "sl", "<cmd>wincmd l<CR>", { desc = "Move to right split" })
+map("n", "sk", "<cmd>wincmd k<CR>", { desc = "Move to upper split" })
+map("n", "sc", "<cmd>wincmd c<CR>", { desc = "Close current split" })
+map("n", "sq", "<cmd>quit<CR>", { desc = "Quit split" })
+map("n", "sx", "<cmd>quit<CR>", { desc = "Quit split" })
+
+-- コンフィグ操作
+map("n", "<Leader>vr", function()
+  vim.cmd("source ~/.config/nvim/init.lua")
+  print("✅ config reloaded!")
+end, { noremap = true, silent = true, desc = "Reload config" })
+
+nomap("n", "<Leader>v") -- 既存の <Leader>v を削除
+map("n", "<Leader>vv", "<cmd>edit ~/.config/nvim/lua/mappings.lua<CR>", { noremap = true, silent = true, desc = "Edit mappings" })
+
+-- LSP 定義ジャンプ
+map("n", "<Leader>b", "<cmd>lua vim.lsp.buf.definition()<CR>", { noremap = true, silent = true, desc = "Go to definition" })
+
+-- インサートモード
+map("i", "jk", "<ESC>", { desc = "Escape insert mode" })
+map("i", "jj", "<ESC>", { desc = "Escape insert mode" })
+
+-- ビジュアルモード
+map("v", "x", '"_x', { desc = "Delete without yanking" })
+map("v", "p", '"_dP', { desc = "Paste without overwriting yank" })
