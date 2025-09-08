6f0a97a2c070c2aa319dbdb90a985d088f68ee79
Koki Kato <kato@shanord.com>
2025-09-08 17:07:23 +0900

    :+1: feat: fish設定の修正と`gh_watch`の改善
    - `gh_watch.fish`でワークフロー名のクオート追加とデフォルト間隔コメントを修正
    - `config.fish`の通知設定で、画像パスのクオートを削除

--- fish/.config/fish/functions/gh_watch.fish
+++ fish/.config/fish/functions/gh_watch.fish
@@ -5,13 +5,13 @@ function gh_watch
     end
 
     set workflow_name $argv[1]
-    set interval 5  # デフォルト間隔は10秒
+    set interval 5  # デフォルト間隔は5秒
 
     if test (count $argv) -ge 2
         set interval $argv[2]
     end
 
-    set run_id (gh run list --workflow $workflow_name --limit 1 --json databaseId --jq ".[0].databaseId")
+    set run_id (gh run list --workflow "$workflow_name" --limit 1 --json databaseId --jq '.[0].databaseId')
 
     if test -z "$run_id"
         echo "エラー: '$workflow_name'という名前のワークフローの実行が見つかりませんでした。"
